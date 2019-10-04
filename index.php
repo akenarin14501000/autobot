9385fd7fa435bce878f229871aed49792e8ebabf
akenarin2297@gmail.com <akenarin2297@gmail.com>
2019-10-04 15:50:41 +0700

    akenarin

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $host='ec2-54-83-9-169.compute-1.amazonaws.com';
 $dbname='d5rut4lfvlp4ar';
 $user='isnivawtdytcrl';
 $pass='fab82d419311ad5a75b8c1719c6acc014ee88e6f7954c3bfe7f134401d838587';
-$connection=new PDO("pgsql:host=$host;dbname=$dbname0",$user,$pass);
+$connection=new PDO("pgsql:host=$host;dbname=$dbname",$user,$pass);
 $result=$connection->query("select * from appiontment");
 
 if ($result!=null){
