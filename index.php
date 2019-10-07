14c4fd3429c051151fe599fb4fca99882e18cc44
akenarin2297@gmail.com <akenarin2297@gmail.com>
2019-10-07 15:58:02 +0700

    a

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once('./vendor/autoload.php');
 #Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
 
 $channel_token='TUChny/ZBXe1rsUoqQyXj6rHNTN+efSNtWn4W65LGh4f96G/xnRomNi0A4iKBhZ71sa40XxbqP2hzk/AvG2bLNpMW4ITGmfyrAgDl5GwTvRCp8vxSPX40Af/Pi8tIBjVZlLATwfGxs+QygxYaCTFMQdB04t89/1O/w1cDnyilFU=';
 $channel_secret='41941150e071e4009e1b8f5c93e36a7a';
@@ -20,14 +20,14 @@ if(!is_null($events['events'])){
 		//if($event['type']=='message'){
 			//Get replyToken
 			$replyToken=$event['replyToken'];
-			$originalContentUrl='https://www.beartai.com/wp-content/uploads/2017/11/22886328_10208432809332575_6674604265514277172_n.jpg';
-			$previewImageUrl='https://www.beartai.com/wp-content/uploads/2017/11/22886328_10208432809332575_6674604265514277172_n.jpg';
+			$packageId=1;
+			$stickerId=410;
 			$httpClient=new CurlHTTPClient($channel_token);
 			$bot=new LINEBot($httpClient,array('channelSecret'=>$channel_secret));	
-			$textMessageBuilder=new ImageMessageBuilder($originalContentUrl,$previewImageUrl);
+			$textMessageBuilder=new StickerMessageBuilder($packageId,$stickerId);
 			$response=$bot->replyMessage($replyToken,$textMessageBuilder);
 		//}
 	}
 }
 
-echo 'Send Image';
+echo 'Send Sticker';
--- send image to line api.php
+++ send image to line api.php
@@ -4,7 +4,7 @@ require_once('./vendor/autoload.php');
 #Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 
 $channel_token='TUChny/ZBXe1rsUoqQyXj6rHNTN+efSNtWn4W65LGh4f96G/xnRomNi0A4iKBhZ71sa40XxbqP2hzk/AvG2bLNpMW4ITGmfyrAgDl5GwTvRCp8vxSPX40Af/Pi8tIBjVZlLATwfGxs+QygxYaCTFMQdB04t89/1O/w1cDnyilFU=';
 $channel_secret='41941150e071e4009e1b8f5c93e36a7a';
@@ -17,18 +17,17 @@ if(!is_null($events['events'])){
 	//loop through each event
 	foreach($events['events'] as $event){
 		//line api send a lot of event type,we interested in message only.
-		
+		//if($event['type']=='message'){
 			//Get replyToken
-			 $replyToken=$event['replyToken'];
+			$replyToken=$event['replyToken'];
 			$originalContentUrl='https://www.beartai.com/wp-content/uploads/2017/11/22886328_10208432809332575_6674604265514277172_n.jpg';
 			$previewImageUrl='https://www.beartai.com/wp-content/uploads/2017/11/22886328_10208432809332575_6674604265514277172_n.jpg';
-	
 			$httpClient=new CurlHTTPClient($channel_token);
 			$bot=new LINEBot($httpClient,array('channelSecret'=>$channel_secret));	
 			$textMessageBuilder=new ImageMessageBuilder($originalContentUrl,$previewImageUrl);
 			$response=$bot->replyMessage($replyToken,$textMessageBuilder);
-		
+		//}
 	}
 }
 
-echo 'Image';
+echo 'Send Image';
