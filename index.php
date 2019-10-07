f52a4b7078093c0431cac9a2aaa4402cea3d729c
akenarin2297@gmail.com <akenarin2297@gmail.com>
2019-10-07 10:20:10 +0700

    a1

--- index.php
+++ index.php
@@ -1,14 +1,38 @@
 <?php
-$host='ec2-54-83-9-169.compute-1.amazonaws.com';
-$dbname='d5rut4lfvlp4ar';
-$user='isnivawtdytcrl';
-$pass='fab82d419311ad5a75b8c1719c6acc014ee88e6f7954c3bfe7f134401d838587';
-$connection=new PDO("pgsql:dbname=$dbname;host=$host", $user, $pass);
-$result=$connection->query("select * from appiontment");
+require_once('./vender/autoload.php');
 
-if ($result!=null){
-	echo "OK";
-} else {
-	echo "สมน้ำหน้า";
+#Namespace
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot
+use \LINE\LINEBot\MessageBuilder\TextMeaasgeBuilder;
+
+$channel_token='1v2OUa9tuMIiDhEg57ANbsRaBDbBGP9nlCC+Dpvt5HrsQ+LqcrImWPUBkH8re/pwqxv56d15kZeMoU/vQ0zuzPFlbhFM7AhRMZwLrSkLdcjbFurwXGOyHLt8MdgzLfAe7r0BsQV5cATlUanW3OgJewdB04t89/1O/w1cDnyilFU=';
+$channel_secret='9b2c7349ea939ef723a3cd453d774c86';
+
+#Get message from line api
+$content =file_get+contents('php://input');
+$events=json_decode($content,true);
+
+if(!is_null($events['events'])){
+	//loop through each event
+	foreach($events['event'] as $event){
+		//line api send a lot of event type,we interested in message only.
+		if($event['type']=='message'){
+			switch($event['message']['type']){
+			 case'test';
+			//Get replyToken
+            $replyToken=$event['replyToken'];
+			//reply message
+			$respMessage='Hello,your message is'.$event['message']['type'];
+			$httpClient=new CurlHTTPClient()$channel_token;
+			$bot=new LINEBot($httpClient,array('channelSecret'=>$channel_secret));
+			$textMessageBuilder=newTextMessageBuilder($respMessage);
+			$response=$bot->replyMessage($replyToken,$textMessageBuilder);
+			break;
+			}
+			
+		}
+	}
 }
 
+echo 'OK';
---
+++ index1.php
@@ -0,0 +1,14 @@
+<?php
+$host='ec2-54-83-9-169.compute-1.amazonaws.com';
+$dbname='d5rut4lfvlp4ar';
+$user='isnivawtdytcrl';
+$pass='fab82d419311ad5a75b8c1719c6acc014ee88e6f7954c3bfe7f134401d838587';
+$connection=new PDO("pgsql:dbname=$dbname;host=$host", $user, $pass);
+$result=$connection->query("select * from appiontment");
+
+if ($result!=null){
+	echo "OK";
+} else {
+	echo "สมน้ำหน้า";
+}
+
