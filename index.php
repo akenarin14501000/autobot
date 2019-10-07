bb3281a5c7d0e59d4006e1c510a0e5490cf4e269
akenarin2297@gmail.com <akenarin2297@gmail.com>
2019-10-07 10:58:54 +0700

    a2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('./vender/autoload.php');
 
 #Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot
+use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMeaasgeBuilder;
 
 $channel_token='TUChny/ZBXe1rsUoqQyXj6rHNTN+efSNtWn4W65LGh4f96G/xnRomNi0A4iKBhZ71sa40XxbqP2hzk/AvG2bLNpMW4ITGmfyrAgDl5GwTvRCp8vxSPX40Af/Pi8tIBjVZlLATwfGxs+QygxYaCTFMQdB04t89/1O/w1cDnyilFU=';
@@ -18,19 +18,24 @@ if(!is_null($events['events'])){
 	foreach($events['event'] as $event){
 		//line api send a lot of event type,we interested in message only.
 		if($event['type']=='message'){
-			switch($event['message']['type']){
-			 case'test';
 			//Get replyToken
-            $replyToken=$event['replyToken'];
-			//reply message
-			$respMessage='Hello,your message is'.$event['message']['type'];
-			$httpClient=new CurlHTTPClient()$channel_token;
-			$bot=new LINEBot($httpClient,array('channelSecret'=>$channel_secret));
-			$textMessageBuilder=newTextMessageBuilder($respMessage);
-			$response=$bot->replyMessage($replyToken,$textMessageBuilder);
+			 $replyToken=$event['replyToken'];
+			switch($event['message']['type']){
+			 case'image';
+			 $messageID=$event['message']['id'];
+           			//reply message
+			$respMessage='Hello,your image ID is'.$messageID;
+			break;
+			default:
+			$respMessage='Please send image only';
+			
+			
 			break;
 			}
-			
+			$httpClient=new CurlHTTPClient($channel_token);
+			$bot=new LINEBot($httpClient,array('channelSecret'=>$channel_secret));	
+			$textMessageBuilder=newTextMessageBuilder($respMessage);
+			$response=$bot->replyMessage($replyToken,$textMessageBuilder);
 		}
 	}
 }
