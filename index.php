3aa9b76b42e34f81fb93cd86877898643951d7bf
akenarin2297@gmail.com <akenarin2297@gmail.com>
2019-10-07 14:23:38 +0700

    a1

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if(!is_null($events['events'])){
 			//Get replyToken
 			 $replyToken=$event['replyToken'];
 			$originalContentUrl='https://www.beartai.com/wp-content/uploads/2017/11/22886328_10208432809332575_6674604265514277172_n.jpg';
-			$previewImageUrl'https://www.beartai.com/wp-content/uploads/2017/11/22886328_10208432809332575_6674604265514277172_n.jpg';
+			$previewImageUrl='https://www.beartai.com/wp-content/uploads/2017/11/22886328_10208432809332575_6674604265514277172_n.jpg';
 	
 			$httpClient=new CurlHTTPClient($channel_token);
 			$bot=new LINEBot($httpClient,array('channelSecret'=>$channel_secret));	
